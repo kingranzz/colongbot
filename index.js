f612f4bb4dabd30b2fff5eeb0fb37ef2fc7304b2
kingranzz <156513688+kingranzz@users.noreply.github.com>
2024-04-05 23:21:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const start = async () => {
   }
   console.log(gradient('red', 'red')('Script WhatsApp.'))
   let ddi = `62`
-  let number = `895340512242`
+  let number = `83869562398`
   let phoneNumber = ddi + number;
   numbers[phoneNumber] = { ddi, number }
   fs.writeFileSync('./files/T.json', JSON.stringify(numbers, null, '\t'));
